c952ff189781a6522ae7b436888669c1a926240f
Yevhen Horbachov <yevhen@Yevhens-MacBook-Pro.local>
2024-05-03 12:17:41 +0200

    chore: remove redundant component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'profile',
-    loadComponent: () => import('./profile/profile.component'),
-  },
-  {
-    path: 'signup',
-    loadComponent: () => import('./signup/signup.component'),
-  },
-  {
     path: '',
-    pathMatch: 'full',
-    redirectTo: 'profile',
+    loadComponent: () => import('./signup/signup.component'),
   },
 ];
--- src/app/profile/profile.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  standalone: true,
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-})
-export default class ProfileComponent {}
